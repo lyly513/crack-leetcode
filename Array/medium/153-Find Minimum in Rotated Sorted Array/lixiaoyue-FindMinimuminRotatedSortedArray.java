f970bcea337776f8ff934423463bc7ba4b172ba2
Li Xiaoyue <lixiaoyue.314@gmail.com>
2016-07-04 23:21:27 +0800

    array-medium

---
+++ Array/medium/153-Find Minimum in Rotated Sorted Array/lixiaoyue-FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,18 @@
+public class Solution {
+    
+    public int findMin(int[] nums) {
+   
+        int front = 0;
+        int rear = nums.length - 1;
+
+        while (front < rear) {
+            int mid = (front + rear) / 2;
+            if (nums[mid] < nums[rear]) rear = mid;
+            else front = mid + 1;
+            
+        }
+        
+        return nums[front];
+    }
+    
+}
